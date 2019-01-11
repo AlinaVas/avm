179a63816a88588dfe4ffebf8a56ac0ecb1e41e2
Alina Fesyk <fesyk.alina@gmail.com>
2019-01-11 11:39:54 +0200

    Parser: initial commit

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include "Lexer.h"
+#include "Parser.h"
+//#include "AbstractVM.hpp"
 
 int main(int ac, char **av) {
 
+//	AbstractVM avm;
 	Lexer lr;
 //	try {
 	lr.getInput(ac, av);
 //	} catch (std::exception &e) {
 //		std::cout << e.what() << std::endl;
 //	}
+	Parser pr(lr.getTokens());
 
 }

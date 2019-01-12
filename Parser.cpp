23457d1755bc5bd3d91028bfd630b554a372b140
Alina Fesyk <fesyk.alina@gmail.com>
2019-01-12 17:12:17 +0200

    Parser: vector of function pointers for operational commands added

--- Parser.cpp
+++ Parser.cpp
@@ -1,6 +1,19 @@
 #include "Parser.h"
 
-Parser::Parser(std::vector<Token> tokens) : _tokens(std::move(tokens)) {}
+Parser::Parser(std::vector<Token> tokens) : _tokens(std::move(tokens)) {
+
+	_commands.push_back(&Parser::push);
+	_commands.push_back(&Parser::pop);
+	_commands.push_back(&Parser::dump);
+	_commands.push_back(&Parser::asrt);
+	_commands.push_back(&Parser::add);
+	_commands.push_back(&Parser::sub);
+	_commands.push_back(&Parser::mul);
+	_commands.push_back(&Parser::div);
+	_commands.push_back(&Parser::mod);
+	_commands.push_back(&Parser::print);
+	_commands.push_back(&Parser::quit);
+}
 
 Parser::Parser(Parser const &rhs) {
 	*this = rhs;
@@ -22,12 +35,39 @@ Parser::initParsing() {
 
 	for (size_t i = 0; i < _tokens.size(); i++) {
 
-		if (_tokens[i].commandType == PUSH)
-			push(i);
+		if (_tokens[i].commandType == EXIT)
+			break;
+		if (_tokens[i].commandType != EMPTY)
+			(this->*_commands[_tokens[i].commandType])(i);
+
 	}
+	std::cout << GREEN << "[=== Program exit ===]" << RESET << std::endl;
 }
 
 void
 Parser::push(size_t &i) {
 	_stack.push_front(Factory().createOperand(_tokens[i].operandType, _tokens[i].operandValue));
+}
+
+void Parser::pop(size_t &) {}
+
+void Parser::dump(size_t &) {}
+
+void Parser::asrt(size_t &) {}
+
+void Parser::add(size_t &) {}
+
+void Parser::sub(size_t &) {}
+
+void Parser::mul(size_t &) {}
+
+void Parser::div(size_t &) {}
+
+void Parser::mod(size_t &) {}
+
+void Parser::print(size_t &) {}
+
+void Parser::quit(size_t &) {
+
+
 }
--- Parser.h
+++ Parser.h
@@ -4,8 +4,16 @@
 #include "Factory.hpp"
 #include "Lexer.h"
 
+#define RESET   "\033[0m"
+#define GREEN	"\033[32m"
+#define RED     "\033[31m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
 
 class Parser {
+
+	typedef void (Parser::*funcPtr)(size_t &);
 
 public:
 	Parser() = delete;
@@ -16,9 +24,22 @@ public:
 
 	void	initParsing();
 	void 	push(size_t &);
+	void	pop(size_t &);
+	void	dump(size_t &);
+	void	asrt(size_t &);
+	void	add(size_t &);
+	void	sub(size_t &);
+	void	mul(size_t &);
+	void	div(size_t &);
+	void	mod(size_t &);
+	void	print(size_t &);
+	void	quit(size_t &);
+
 
 private:
 	std::vector<Token>			_tokens;
+	std::vector<funcPtr>		_commands;
+
 	std::list<IOperand const*>	_stack;
 };
 
